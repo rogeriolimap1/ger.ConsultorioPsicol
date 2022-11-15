8a3f1bf1a97be653441fa9de04fda48851fafd29
TENENTE <rogerio.lima@estudante.iftm.edu.br>
2022-11-15 19:10:53 -0300

    01

---
+++ src/test/java/com/LIMA/ger/consultorioPsicol/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LIMA.ger.consultorioPsicol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
