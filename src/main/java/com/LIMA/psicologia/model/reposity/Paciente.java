befa6e1097b9522202fa580bb31e40f6aab59992
TENENTE <rogerio.lima@estudante.iftm.edu.br>
2022-11-15 21:13:16 -0300

    2

---
+++ src/main/java/com/LIMA/psicologia/model/reposity/Paciente.java
@@ -0,0 +1,29 @@
+package com.LIMA.psicologia.model.reposity;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+ 
+@Data @NoArgsConstructor @AllArgsConstructor
+
+public class Paciente {
+	private int cpf;
+	private String nome;
+	private Date dataNascimento;
+	private String nomeMae;
+	private String nomePai;
+	private int rg;
+	private int foneCelular;
+	private int foneFixo;
+	private String email;
+	private String enderecoRuaNumero;
+	private String cidade;
+	private String estado;
+	private int cep;
+	private String tipoContratoSessao;
+	private int numeroSessoesMes;
+	private BigDecimal valor;
+	private LocalDateTime dataContrato = LocalDateTime.now();
+}
