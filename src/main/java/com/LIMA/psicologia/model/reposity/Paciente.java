8bb858e71b059e71a4d08fc9fe5856a1f30462b2
iure11 <iurematheus11@gmail.com>
2022-11-17 19:47:33 -0300

    01

--- src/main/java/com/LIMA/psicologia/model/reposity/Paciente.java
+++ src/main/java/com/LIMA/psicologia/model/reposity/Paciente.java
@@ -5,7 +5,8 @@ import java.sql.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
- 
+
+@Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 
 public class Paciente {
